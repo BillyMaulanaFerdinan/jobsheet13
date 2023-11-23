814da0eea50c4cb2787ee6d21470f0b4538a4d3b
BillyMaulanaFerdinan <billy.maulana2020@gmail.com>
2023-11-23 10:41:22 +0700

    First Fungsi

---
+++ TerimaKasih_06.java
@@ -0,0 +1,10 @@
+public class TerimaKasih_06 {
+
+    public static void UcapanTerimaKasih() {
+        System.out.println("Thank you for being the best teacher in the world.\n"+
+        "You inpired in me a love for learning and made me feel like i could ask you anything");
+    }
+    public static void main(String[] args) {
+        UcapanTerimaKasih();
+    }
+}
