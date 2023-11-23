432e0e5cd52ef791c863ad04a2246c1696d2db5e
BillyMaulanaFerdinan <billy.maulana2020@gmail.com>
2023-11-23 11:11:40 +0700

    Modif UcapanTambahan

--- TerimaKasih_06.java
+++ TerimaKasih_06.java
@@ -4,12 +4,12 @@ public class TerimaKasih_06 {
         System.out.println("Thank you for being the best teacher in the world.\n"+
         "You inpired in me a love for learning and made me feel like i could ask you anything");
     }
+    public static void UcapanTambahan(String ucapan) {
+        System.out.println(ucapan);
+    }
     public static void main(String[] args) {
-        //Tanpa Menggunakan Fungsi
-        System.out.println("Thank you for being the best teacher in the world.\n"+
-        "You inpired in me a love for learning and made me feel like i could ask you anything");;
-        
-        //Menggunakan Fungsi
         UcapanTerimaKasih();
+        String ucapan = "Terima kasih Pak.. Bu.. Semoga Sehat Selalu";
+        UcapanTambahan(ucapan);
     }
 }
